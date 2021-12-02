e68f6a24d028c61bb23aa282f1c5ef28ad0f96cc
david reinfurt <reinfurt@o-r-g.com>
2021-12-02 14:11:27 -0500

    add watch icon assets

---
+++ watch WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  watch WatchKit Extension
+//
+//  Created by david reinfurt on 12/2/21.
+//  Copyright © 2021 o-r-g. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
