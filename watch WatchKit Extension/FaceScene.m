743f808ccd4c0614ebb2f7729196966a81d2a664
david reinfurt <reinfurt@o-r-g.com>
2021-12-03 18:14:18 -0500

    cleanup

--- watch WatchKit Extension/FaceScene.m
+++ watch WatchKit Extension/FaceScene.m
@@ -19,41 +19,9 @@
 
 
 -(void)setupScene {
-    
-	SKNode *face = [self childNodeWithName:@"Face"];
-	
-	SKSpriteNode *hourHand = (SKSpriteNode *)[face childNodeWithName:@"Hours"];
-	SKSpriteNode *minuteHand = (SKSpriteNode *)[face childNodeWithName:@"Minutes"];
-	
-	SKSpriteNode *hourHandInlay = (SKSpriteNode *)[hourHand childNodeWithName:@"Hours Inlay"];
-	SKSpriteNode *minuteHandInlay = (SKSpriteNode *)[minuteHand childNodeWithName:@"Minutes Inlay"];
-	
-	SKSpriteNode *secondHand = (SKSpriteNode *)[face childNodeWithName:@"Seconds"];
-	SKSpriteNode *colorRegion = (SKSpriteNode *)[face childNodeWithName:@"Color Region"];
-	SKSpriteNode *staticImageLayer = (SKSpriteNode *)[[face childNodeWithName:@"Image Root"] childNodeWithName:@"Static Image Layer"];
 
-	hourHand.color = self.handColor;
-	hourHand.colorBlendFactor = 1.0;
-	
-	minuteHand.color = self.handColor;
-	minuteHand.colorBlendFactor = 1.0;
-	
-	secondHand.color = self.secondHandColor;
-	secondHand.colorBlendFactor = 1.0;
-	
-	self.backgroundColor = self.faceBackgroundColor;
-	
-	colorRegion.color = self.colorRegionColor;
-	colorRegion.colorBlendFactor = 1.0;
-	
-	staticImageLayer.color = self.textColor;
-	staticImageLayer.colorBlendFactor = 1.0;
-	
-	hourHandInlay.color = self.inlayColor;
-	hourHandInlay.colorBlendFactor = 1.0;
-	
-	minuteHandInlay.color = self.inlayColor;
-	minuteHandInlay.colorBlendFactor = 1.0;
+    // add dot here 
+
 }
 
 
@@ -67,18 +35,20 @@
 	[self updateHands];
 }
 
--(void)updateHands {
+
+
+
+
+
+- (void)updateHands {
     
-#if PREPARE_SCREENSHOT
-	NSDate *now = [NSDate dateWithTimeIntervalSince1970:32760+27]; // 10:06:27am
-#else
+    // update lissajous here
+
 	NSDate *now = [NSDate date];
-#endif
 	NSCalendar *calendar = [NSCalendar currentCalendar];
 	NSDateComponents *components = [calendar components:(NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond| NSCalendarUnitNanosecond) fromDate:now];
 	
-	SKNode *face = [self childNodeWithName:@"Face"];
-	
+	SKNode *face = [self childNodeWithName:@"Face"];	
 	SKNode *hourHand = [face childNodeWithName:@"Hours"];
 	SKNode *minuteHand = [face childNodeWithName:@"Minutes"];
 	SKNode *secondHand = [face childNodeWithName:@"Seconds"];
